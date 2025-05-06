5246a1d22abedbff6412c3f004a44288f113cc86
danilgubarev <danilgubarev9804@gmail.com>
2025-05-06 11:57:22 +0300

    fix bug with visibility slug

--- portfolio/gallery/admin.py
+++ portfolio/gallery/admin.py
@@ -4,16 +4,16 @@ from .models import Category, Album, Info, Experience, AlbumPhoto, AboutMe
 # Register your models here.
 admin.site.register([Info, Experience, AboutMe])
 
-
-
 class AlbumPhotoInline(admin.TabularInline):
     model = AlbumPhoto
     extra = 0
 
+@admin.register(Album)
+
 class AlbumAdmin(admin.ModelAdmin):
     inlines = [AlbumPhotoInline]
-    
-@admin.register(Album)
+    exclude = ["slug"]
+
 
 class CategoryAdmin(admin.ModelAdmin):
     exclude = ["slug"]
