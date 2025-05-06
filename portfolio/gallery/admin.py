5357e6e963f1b784d8002062e82ff3a3b5564e2a
danilgubarev <danilgubarev9804@gmail.com>
2025-05-06 10:04:41 +0300

    hid the slug field in the admin panel

--- portfolio/gallery/admin.py
+++ portfolio/gallery/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Category, Album, Info, Experience, AlbumPhoto, AboutMe
 # Register your models here.
-admin.site.register([Category, Info, Experience, AboutMe])
+admin.site.register([Info, Experience, AboutMe])
 
 
 
@@ -10,6 +10,12 @@ class AlbumPhotoInline(admin.TabularInline):
     model = AlbumPhoto
     extra = 0
 
-@admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     inlines = [AlbumPhotoInline]
+    exclude = ["slug"]
+@admin.register(Album)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ["slug"]
+admin.site.register(Category, CategoryAdmin)
+
